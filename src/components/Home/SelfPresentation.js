2a9b96b2f7be9ac37974a69da049adf9cff1b4e6
bazzoneto <developerpedropbazzo@gmail.com>
2025-03-08 18:01:32 -0300

    add fix name

--- src/components/Home/SelfPresentation.js
+++ src/components/Home/SelfPresentation.js
@@ -18,7 +18,7 @@ const SelfPresentation = () => (
         <S.Hello>Prazer! Me chamo Pedro mas pode me chama de Bjorn.</S.Hello>
         <S.Text>
           Trabalho com tecnologia desde 2010 especificamente com bancos a mais de dez anos atualmente sou 
-          <span className="gotham-bold"> Especialista <span className="gotham" style={{color: '#ff0000'}}>Java</span></span> 
+          <span className="gotham-bold"> Especialista <span className="gotham" style={{color: '#ff0000'}}>Java </span></span> 
           no <span className="gotham-bold" style={{color: '#4169e1'}}> Agibank </span>.
           As minhas intenções com esse blog é documentar meus aprendizados 
           e compartilhar com você o conhecimento obtido nessa jornada infinita do roadmap em tecnologia.
