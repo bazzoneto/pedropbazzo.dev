427ba11d0352bc63cabdb4053c67cfaa11ef2a3a
bazzoneto <developerpedropbazzo@gmail.com>
2025-03-08 19:44:18 -0300

    add fix about

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -10,32 +10,10 @@ const About = () => (
     <div className="container">
       <Title>Sobre mim</Title>
       <Text>
-        💻 Comecei minha jornada em 2014, onde atuava em projetos com Spring Framework escritos em Java 7 para o Back-end e 
-        uma salada de fruta de ChaplinJs Controllers com BackboneJs MVVM no Front-end 
-        (acho que nessa época o termo FullStack nem era tão falado assim, mas sempre gostei de conhecer os dois mundos).
+        💻 Comecei minha jornada em 2011, o Java mudou minha vida e pode mudar a sua, apenas comece...
       </Text>
       <Text>
-        Dois anos depois veio a Era dos monolítos com Spring Boot + Java 8, provendo REST APIs para SPAs em Angular Js. 
-        Em 2017 minha última aventura na Software House foi explorando o mundo mobile, onde trabalhei como desenvolvedor Android e iOS.
-      </Text>
-      <Text>
-        Em dezembro de 2017 entrei para o Mercado Livre, onde comecei a explorar o mundo de microserviços
-        em larga escala. Pude participar do desenvolvimento de um produto muito chave para a companhia que
-        conta com mais de 100 microserviços, 300K RPM de leitura no serviço principal, 30K usuários concorrentes ativos, 
-        gestão de 1BI documentos fiscais e mantendo um excelente UPTIME e baixa latência.
-      </Text>
-      <Text>
-        Hoje em dia meu foco não é mais tanto contribuir ativamente com código, mas sim uma contribuição mais estratégica
-        onde controlo o roadmap técnico, com visão mais longo prazo, garantindo alinhamento entre os Tech Leads e 
-        direcionando as decisões arquiteturais para cumprir com os objetivos da empresa. Além disso, executo um papel 
-        fundamental em mentoria para os desenvolvedores e os Tech Leads para que desenvolvam suas skills técnicas, 
-        garantindo a construição de serviços escaláveis na Cloud.
-      </Text>
-      <Text>
-        No meu período livre, trabalho na minha Startup Contrata Show e compartilhando conteúdo aqui.
-      </Text>
-      <Text>
-        Tech Lead from Brasil 🇧🇷
+        Especialista Java 🇧🇷
       </Text>
       <Text>
         💬 Converse comigo sobre: 
@@ -45,8 +23,8 @@ const About = () => (
       </Text>
       
       <Quote>
-      “I love programming. I enjoy the challenge to not only make a working program, but to do so with style. Programming is like poetry. It conveys a message, not only to the computer, but to those who modify and use your program. With a program, you build your own world with your own rules. You create your world according to your conception of both the problem and the solution. Masterful programmers create worlds with programs that are clear and succinct, much like a poem or essay.”
-      Jonathan Bartlett (Programming from the Ground Up, p. 7).
+      “Nada no mundo se compara à persistência. Nem o talento; não há nada mais comum do que homens malsucedidos e com talento. Nem a genialidade; a existência de gênios não recompensados é quase um provérbio. Nem a educação; o mundo está cheio de negligenciados educados. A persistência e determinação são, por si sós, onipotentes.”
+      Calvin Coolidge (Presidente dos Estados Unidos em 1923).
       </Quote>
       <br/>
       
